3c10292e663975f8b50edd105a3c099a5b6f1e44
Travisrowe <travisrowe18@gmail.com>
2019-03-12 22:27:23 -0500

    Completed first test

--- BinarySearchTree/BinarySearchTree/Main.cpp
+++ BinarySearchTree/BinarySearchTree/Main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <time.h>
 #include <iostream>
+//chrono used for time measurements
+#include <chrono> 
+using namespace std::chrono;
 
 //L2CacheSize  L3CacheSize
 //512 kb       3072 kb
@@ -11,9 +14,10 @@
 // 524,288 / 12 = 43,690 Nodes
 
 #define NUM_NODES 43690 //number of nodes
-#define ID_PAIRS 10000 //number of insert/delete pairs
-#define SEED 2
+#define ID_PAIRS 100000 //number of insert/delete pairs
+#define SEED 99
 //#define DEBUG true
+//#define FIND_SEED true
 
 //swap index a with index b inside vect
 void Swap(vector<int> &vect, int a, int b)
@@ -25,6 +29,22 @@ void Swap(vector<int> &vect, int a, int b)
 
 int main()
 {
+#ifdef FIND_SEED
+	int closestVal = NUM_NODES;
+	int closestSeed;
+	for (int i = 0; i < 100; i++)
+	{
+		srand(i);
+		int difference = abs((int)(NUM_NODES / 2) - rand() % NUM_NODES);
+		if (difference < closestVal)
+		{
+			closestVal = difference;
+			closestSeed = i;
+		}
+	}
+	cout << "Seed: " << closestSeed << "val: " << closestVal << '\n';
+#endif // SEED
+
 #ifdef DEBUG
 
 	srand(SEED);
@@ -96,8 +116,99 @@ int main()
 	std::cout << "\nUT\n";
 	UT.Print();
 
-#else // Time-test program
+#else// Time-test program
+	srand(SEED);
 	
+	std::cout << "Let's begin!\n\n";
+	std::cout << "Generating initial values for trees and insert/delete pairs....\n";
+	vector<int> initialTreeVals(NUM_NODES); //vals the tree has inserted initially
+	vector<int> treeVals(NUM_NODES); //vals the tree has "currently" these are used to select Nodes to delete
+	for (int i = 0; i < NUM_NODES; i++)
+	{
+		initialTreeVals[i] = treeVals[i] = rand() % NUM_NODES;
+	}
+	vector<int> InsertVals(ID_PAIRS);
+	vector<int> DeleteVals(ID_PAIRS);
+	for (int i = 0; i < ID_PAIRS; i++)
+	{
+		//get index to delete, then replaces with new val
+		int index = rand() % NUM_NODES;
+		//store the val to delete
+		DeleteVals[i] = treeVals[index];
+		//generate new val to insert
+		InsertVals[i] = rand() % NUM_NODES;
+		//replace "deleted" val with "inserted" val
+		treeVals[index] = InsertVals[i];
+	}
+	std::cout << "Finished generating random vals!\n";
+	std::cout << "Starting time for Control tree (Cache-unaware BST)....\n";
+
+	/****************************************************************************
+	 * Unaware BST timing *******************************************************
+	****************************************************************************/
+	auto start = high_resolution_clock::now(); //start time
+
+	UnawareBST UT;
+	//create initial tree
+	for (int i = 0; i < NUM_NODES; i++)
+	{
+		UT.Insert(initialTreeVals[i]);
+	}
+	for (int i = 0; i < ID_PAIRS; i++)
+	{
+		//delete first, then insert
+		UT.Delete(DeleteVals[i]);
+		UT.Insert(InsertVals[i]);
+	}
+
+	auto stop = high_resolution_clock::now(); //stop time
+
+	// Subtract stop and start timepoints and 
+	// cast it to required unit. Predefined units 
+	// are nanoseconds, microseconds, milliseconds, 
+	// seconds, minutes, hours. Use duration_cast() 
+	// function. 
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	// To get the value of duration use the count() 
+	// member function on the duration object 
+	cout << "Time for Control tree: " << duration.count() << "microseconds\n";
+
+	/****************************************************************************
+	 * Single Vector Aware BST timing *******************************************
+	****************************************************************************/
+	std::cout << "Starting time for single vector tree...\n";
+	auto start_AT = high_resolution_clock::now(); //start time
+
+	AwareBST AT(NUM_NODES);
+	//create initial tree
+	for (int i = 0; i < NUM_NODES; i++)
+	{
+		if (i % 1000 == 0)
+			cout << i << '\n';
+		AT.Insert(initialTreeVals[i]);
+	}
+	for (int i = 0; i < ID_PAIRS; i++)
+	{
+		if (i % 100 == 0)
+			cout << i << '\n';
+		//delete first, then insert
+		AT.Delete(DeleteVals[i]);
+		AT.Insert(InsertVals[i]);
+	}
+
+	auto stop_AT = high_resolution_clock::now(); //stop time
+
+	// Subtract stop and start timepoints and 
+	// cast it to required unit. Predefined units 
+	// are nanoseconds, microseconds, milliseconds, 
+	// seconds, minutes, hours. Use duration_cast() 
+	// function. 
+	auto duration_AT = duration_cast<microseconds>(stop_AT - start_AT);
+
+	// To get the value of duration use the count() 
+	// member function on the duration object 
+	cout << "Time for Single-Vector, Aware tree: " << duration_AT.count() << "microseconds\n";
 #endif // Debug
 
 }
